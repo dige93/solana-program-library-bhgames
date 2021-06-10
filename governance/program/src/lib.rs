02c15899332c5b5481b1a95ab9d989f601483ebc
Sebastian.Bor <sebastian_bor@hotmail.com>
2021-06-10 18:53:02 +0100

    fix: use actual program address

--- governance/program/src/lib.rs
+++ governance/program/src/lib.rs
@@ -11,7 +11,7 @@ pub mod tools;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("GovernancerdmUu324nahyv33G5poQdLUEZ1nEytDeP");
+solana_program::declare_id!("GovergMfhoNZePj4v86rLXZSN4DeFSLmvKEgWCch1Zuu");
 
 /// Seed prefix for Governance  PDAs
 pub const PROGRAM_AUTHORITY_SEED: &[u8] = b"governance";
