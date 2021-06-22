db3d05c626477aecc3fe4accaf978cfca8533ceb
Sebastian.Bor <sebastian_bor@hotmail.com>
2021-06-22 11:24:11 +0100

    feat: use runtime program_id

--- governance/program/src/lib.rs
+++ governance/program/src/lib.rs
@@ -11,7 +11,5 @@ pub mod tools;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("GovergMfhoNZePj4v86rLXZSN4DeFSLmvKEgWCch1Zuu");
-
 /// Seed prefix for Governance  PDAs
 pub const PROGRAM_AUTHORITY_SEED: &[u8] = b"governance";
